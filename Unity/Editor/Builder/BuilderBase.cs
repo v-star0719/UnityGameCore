2437b1ba72ee59d08c10e1b191a9247028000729
tuqiming <tuqiming@camel4u>
2024-07-10 21:54:55 +0800

    优化

--- Unity/Editor/Builder/BuilderBase.cs
+++ Unity/Editor/Builder/BuilderBase.cs
@@ -2,6 +2,7 @@ using Codice.Client.Common;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
@@ -16,6 +17,10 @@ namespace Kernel.Unity
 
         //对于Windows来说，打包出来是一个文件夹
         protected string packageName;
+        protected BuildReport buildReport;
+
+        protected bool IsSucceed => buildReport.summary.result == BuildResult.Succeeded;
+        protected string OutputPath => buildReport.summary.outputPath;
 
         //如果需要增加步骤，在这里面加
         public void Build()
@@ -25,9 +30,15 @@ namespace Kernel.Unity
                 Debug.LogError($"current active build target is not {buildTarget}, it is {EditorUserBuildSettings.activeBuildTarget}");
                 return;
             }
+
             SetPackageName();
-            //SetScriptingDefineSymbols();
+            SetScriptingDefineSymbols();
             DoBuild();
+
+            if (IsSucceed)
+            {
+                CopyStreamingAssets();
+            }
         }
 
         protected virtual void SetPackageName()
@@ -35,9 +46,48 @@ namespace Kernel.Unity
             packageName = $"{PlayerSettings.productName}_{buildTarget}_version_{DateTime.Now:yy-MM-dd,HH-mm-ss}";
         }
 
+        protected virtual void DoBuild()
+        {
+            //var sceneArray = new string[scenes.Count];
+            //for (var i = 0; i < scenes.Count; i++)
+            //{
+            //    sceneArray[i] = AssetDatabase.GetAssetPath(scenes[i]);
+            //}
+
+            //如果不指定目录，会自动创建在根目录下。如果目录已经有一个包，会更新修改的文件，但是是否会删除用不到的就不知道了
+            var buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = GetScenes().ToArray();
+            buildPlayerOptions.locationPathName = GetLocationPathName();
+            buildPlayerOptions.target = buildTarget;
+            buildPlayerOptions.options = GetOptions();
+            buildPlayerOptions.extraScriptingDefines = GetExtraScriptingDefineSymbols().ToArray();
+
+            buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = buildReport.summary;
+            if(summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+                EditorUtility.RevealInFinder(summary.outputPath);
+            }
+            else
+            {
+                Debug.Log($"Build {summary.result}");
+            }
+        }
+
+        //不建议用这个，会导致unity又编译一次，增加打包时间
         protected virtual void SetScriptingDefineSymbols()
         {
-            PlayerSettings.SetScriptingDefineSymbols(EditorUtils.CurrentNamedBuildTarget, GetExtraScriptingDefineSymbols().ToArray());
+            var list = GetScriptingDefineSymbols();
+            if (list != null)
+            {
+                PlayerSettings.SetScriptingDefineSymbols(EditorUtils.CurrentNamedBuildTarget, list.ToArray());
+            }
+        }
+
+        protected virtual List<string> GetScriptingDefineSymbols()
+        {
+            return null;
         }
 
         //PlayerSettings里的还会存在，这是额外附加的
@@ -68,6 +118,8 @@ namespace Kernel.Unity
         ///如果用D:/123这样的路径，就会把exe生成为123名字的文件，不带扩展名。所有文件都放在D:/目录下。
         protected virtual string GetLocationPathName()
         {
+            //可以通过这个获取工程目录
+            //var path = Path.GetDirectoryName(Application.dataPath);
             return "";
         }
 
@@ -75,34 +127,10 @@ namespace Kernel.Unity
         {
             return BuildOptions.None;
         }
-        
-        protected virtual void DoBuild()
+
+        protected virtual void CopyStreamingAssets()
         {
-            //var sceneArray = new string[scenes.Count];
-            //for (var i = 0; i < scenes.Count; i++)
-            //{
-            //    sceneArray[i] = AssetDatabase.GetAssetPath(scenes[i]);
-            //}
-
-            //如果不指定目录，会自动创建在根目录下。如果目录已经有一个包，会更新修改的文件，但是是否会删除用不到的就不知道了
-            var buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = GetScenes().ToArray();
-            buildPlayerOptions.locationPathName = GetLocationPathName();
-            buildPlayerOptions.target = buildTarget;
-            buildPlayerOptions.options = GetOptions();
-            buildPlayerOptions.extraScriptingDefines = GetExtraScriptingDefineSymbols().ToArray();
-
-            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-            BuildSummary summary = report.summary;
-            if(summary.result == BuildResult.Succeeded)
-            {
-                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-                EditorUtility.RevealInFinder(summary.outputPath);
-            }
-            else if(summary.result == BuildResult.Failed)
-            {
-                Debug.Log("Build failed");
-            }
+            //FileUtil.CopyFileOrDirectory();
         }
     }
 }
