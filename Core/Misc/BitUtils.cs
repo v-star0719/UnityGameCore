548eaf53d55b8d0795375b53a3fcc5cfd93ab1fc
tuqiming <tuqiming@camel4u>
2024-10-16 21:41:28 +0800

    + Bitutils

---
+++ Core/Misc/BitUtils.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+
+namespace Kernel.Core
+{
+    public class BitUtils
+    {
+        public static bool Contains(int n, int place)
+        {
+            return (n & (1 << place)) == 1;
+        }
+
+        public static void Add(ref int n, int place)
+        {
+            n |= (1 << place);
+        }
+
+        public static void Del(ref int n, int place)
+        {
+            n &= ~(1 << place);
+        }
+
+        public static int GetNumberOf1(int n)
+        {
+            int c = 0;
+            while (n != 0)
+            {
+                n = n & (n - 1);
+                c++;
+            }
+            return c;
+        }
+    }
+}
